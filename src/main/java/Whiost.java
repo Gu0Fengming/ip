ef9510a93e99320bb523434b264a4aaf8bafe9ee
Gu0Fengming <e1129782@u.nus.edu>
2025-02-04 22:53:02 +0800

    Add and List

--- src/main/java/Whiost.java
+++ src/main/java/Whiost.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Whiost {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Whiost\nWhat can I do for you?\n";
         System.out.println(greeting);
 
         boolean p = true;
+        ArrayList<String> lst = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
         while (p) {
             String inp = scanner.nextLine();
             if (inp.equals("bye")) {
                 p = false;
                 System.out.println("Bye. Hope to see you again soon!\n");
+            } else if (inp.equals("list")) {
+                for (int i = 0; i < lst.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, lst.get(i));
+                }
             } else {
-                System.out.println(inp);
+                lst.add(inp);
+                System.out.println("added: " + inp);
             }
         }
 
